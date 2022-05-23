7bc750f1ee72238f29112f9a9249460b6bd581c4
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-05-23 18:28:33 +0200

    chore: remove unused webpack plugin

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const isDev = process.env.NODE_ENV === "development";
 
