8279175f71d08d8f55e2216448683fee7aedaa76
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-05-26 08:53:04 +0200

    chore: fix dependabot breaking commitlint

--- aleo-snap/commitlint.config.js
+++ aleo-snap/commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
-}
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (message) => /^Bumps \[.+]  \(.+\) from .+ to .+\.$/m.test(message),
+  ],
+};
