78dc525c7118bcd69eee3b09fc66dcf130f58bc6
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-05-26 09:03:17 +0200

    chore: fix url length breaking commitlint

--- aleo-snap/commitlint.config.js
+++ aleo-snap/commitlint.config.js
@@ -3,4 +3,7 @@ module.exports = {
   ignores: [
     (message) => /^Bumps \[.+]  \(.+\) from .+ to .+\.$/m.test(message),
   ],
+  rules: {
+    'body-max-line-length': [0, 'always'], // Make sure there is never a max-line-length by disabling the rule
+  },
 };
